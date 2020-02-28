ca41e7a1afbe3f4dc0d21d4cf2cd44b56b010483
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2020-02-28 12:00:29 +0100

    initial commit

---
+++ Haverweik/Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour
+{
+	private Color redColor = Color.red;
+	private Color whiteColor = Color.white;
+	private Renderer renderer;
+
+	private int health = 100;
+	private int dmg = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+		renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (health == 0)
+		{
+			StartCoroutine(Dying());
+		}
+    }
+	public int GetHealth()
+	{
+		return health;
+	}
+	public void TakeDamage()
+	{
+		health -= dmg;
+	}
+	IEnumerator Dying()
+	{
+		yield return null;
+		renderer.material.color = redColor;
+		yield return new WaitForSeconds(0.08f);
+		renderer.material.color = whiteColor;
+		yield return new WaitForSeconds(0.08f);
+		renderer.material.color = redColor;
+		yield return new WaitForSeconds(0.08f);
+		Destroy(this.gameObject);
+	}
+
+}
---
+++ Haverweik/Assets/mouseRaycast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseRaycast : MonoBehaviour
+{
+	[SerializeField] private Camera camera;
+	Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (hit.collider.CompareTag("Player")){
+				player.TakeDamage();
+			}
+		}
+    }
+}
